7099c11ff841776ea45f5817b4f96ad7377b4864
skyincyan <zoya.antysheva@gmail.com>
2026-02-28 13:12:54 +0000

    bug fix in multiallelic logic

--- components/phasing_relatedness/phasing_related.py
+++ components/phasing_relatedness/phasing_related.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import os
 import argparse
 import logging
@@ -45,9 +47,7 @@ def extract_genotypes(vcf):
     
     gt_vector = {}
     
-    for idx, row in vcf.iterrows(): 
-        group_id = str(row['POS']) + '_' + row['REF']
-        
+    for idx, row in vcf.iterrows():         
         if ',' in row['ALT']: # multiallelic sites are split into separate ids 
             for num, alt in enumerate(row['ALT'].split(',')):
                 num = num+1
@@ -55,12 +55,12 @@ def extract_genotypes(vcf):
 
                 gt_relabled = row['GT'].split('/')
                 gt_relabeled = [str(int(g == str(num))) for g in gt_relabled]
-                gt_vector[variant_id] = [group_id, '/'.join(gt_relabeled).strip('/')]
+                gt_vector[variant_id] = '/'.join(gt_relabeled).strip('/')
         else:
             variant_id = str(row['POS']) + '_' + row['REF'] + "_" + row['ALT']
-            gt_vector[variant_id] = [group_id, row['GT']]
+            gt_vector[variant_id] = row['GT']
     
-    return pd.DataFrame(gt_vector, index=['group', 'gt']).T
+    return gt_vector
 
 
 def genotype_split(genotype): 
@@ -194,7 +194,7 @@ def phase_variant_multiallelic(patient, mother, father, phased_variants, unphase
     if mother_state != 1: 
         mother_phased['I'], mother_phased['N'] = mother_state // 2, mother_state // 2
     if father_state != 1: 
-        father_phased['I'], father_phased['N'] = father_state // 2, father_phased // 2
+        father_phased['I'], father_phased['N'] = father_state // 2, father_state // 2
 
     if (len(father_phased) > 0) & (len(mother_phased) > 0) & (len(patient_phased) > 0): # all phased, early exit 
         phased_variants[unphased_variant_name] = patient_phased['M'], patient_phased['F'], mother_phased['I'], mother_phased['N'], father_phased['I'], father_phased['N']
@@ -207,12 +207,12 @@ def phase_variant_multiallelic(patient, mother, father, phased_variants, unphase
             patient_phased['M'], patient_phased['F'] =  mother_phased['I'], patient_state // 2
         else: 
             phased_variants.pop(phased_variant_name)
-            logging.warning(f'{variant_name} genotype in child is {patient}, parents are {father}, {mother}. One of the genotypes is incorrect. Additional checks are required')
+            logging.warning(f'{unphased_variant_name} genotype in child is {patient}, parents are {father}, {mother}. One of the genotypes is incorrect. Additional checks are required')
             return phased_variants
     
     if patient_state > max_patient_state: # de novo variant appearance; 2/1 anynot1 anyone1 was already handled by previous logic. Other can't be solved
         phased_variants.pop(phased_variant_name)
-        logging.warning(f'{variant_name} genotype in child is {patient}, parents are {father}, {mother}. Variant is de novo or one of the genotypes is incorrect. Additional checks are required')
+        logging.warning(f'{unphased_variant_name} genotype in child is {patient}, parents are {father}, {mother}. Variant is de novo or one of the genotypes is incorrect. Additional checks are required')
         return phased_variants
                 
     # assign those that do no have heterozygous, using recieved phasing, assume that there are no de novo variants 
@@ -431,8 +431,8 @@ def determine_deletions_source(ids_list, genotypes_extracted, all_variants, ploi
             deletion_source = 'proxy'
     else:
         patient_id, mother_id, father_id = ids_list[0], ids_list[1], ids_list[2]
-        mother_mismatch = assess_mismatches(all_variants, genotypes_extracted[patient_id]['gt'], genotypes_extracted[mother_id]['gt'])
-        father_mismatch = assess_mismatches(all_variants, genotypes_extracted[patient_id]['gt'], genotypes_extracted[father_id]['gt'])
+        mother_mismatch = assess_mismatches(all_variants, genotypes_extracted[patient_id], genotypes_extracted[mother_id])
+        father_mismatch = assess_mismatches(all_variants, genotypes_extracted[patient_id], genotypes_extracted[father_id])
         
         if father_mismatch > mother_mismatch: 
             deletion_source = father_id
@@ -443,11 +443,14 @@ def determine_deletions_source(ids_list, genotypes_extracted, all_variants, ploi
                 deletion_source = father_id
             elif ploidy[mother_id] == 1: 
                 deletion_source = mother_id
-            else: 
+            elif father_mismatch == 0: 
+                logging.warning(f'Source of deleted copy in {patient_id} not identified. Patient genotype can be explained with parents variants. Might be false positive deletion call. Processing as diploid.')
+                return False, 'no' 
+            else:
                 logging.warning(f'Source of deleted copy in {patient_id} not identified. All parents have 2 copies and equal number of mistmatching variants. Randomly assigning father copy as lost copy')
                 deletion_source = father_id
 
-    return deletion_source 
+    return True, deletion_source 
 
 
 def phase_trio_mode(patient_id, mother_id, father_id, genotypes_extracted, all_variants, deletion_source, is_patient_with_deletion): 
@@ -457,10 +460,10 @@ def phase_trio_mode(patient_id, mother_id, father_id, genotypes_extracted, all_v
     # TRIO PHASING 
     phased_variants = {}
     for variant_id in all_variants: 
-        phased_variants = phase_variant(return_genotype(variant_id, genotypes_extracted[patient_id]['gt'], is_patient_with_deletion=is_patient_with_deletion),
-                                            return_genotype(variant_id, genotypes_extracted[mother_id]['gt'], 
+        phased_variants = phase_variant(return_genotype(variant_id, genotypes_extracted[patient_id], is_patient_with_deletion=is_patient_with_deletion),
+                                            return_genotype(variant_id, genotypes_extracted[mother_id], 
                                                             is_deletion_source=(mother_id == deletion_source)),
-                                            return_genotype(variant_id, genotypes_extracted[father_id]['gt'],
+                                            return_genotype(variant_id, genotypes_extracted[father_id],
                                                             is_deletion_source=(father_id == deletion_source)),
                                             phased_variants, variant_id)
 
@@ -475,11 +478,11 @@ def phase_trio_mode(patient_id, mother_id, father_id, genotypes_extracted, all_v
             continue 
         # if not, use multiallelic phasing to correct unphased variant
         phased, unphased = phased_alone[0], [v for v in v_in_group if v not in phased_variants.keys()][0]
-        phased_variants = phase_variant_multiallelic(return_genotype(unphased, genotypes_extracted[patient_id]['gt'],
+        phased_variants = phase_variant_multiallelic(return_genotype(unphased, genotypes_extracted[patient_id],
                                                                      is_patient_with_deletion=is_patient_with_deletion),
-                                            return_genotype(unphased, genotypes_extracted[mother_id]['gt'], 
+                                            return_genotype(unphased, genotypes_extracted[mother_id], 
                                                             is_deletion_source=(mother_id == deletion_source)),
-                                            return_genotype(unphased, genotypes_extracted[father_id]['gt'],
+                                            return_genotype(unphased, genotypes_extracted[father_id],
                                                             is_deletion_source=(father_id == deletion_source)), phased_variants, unphased, phased)   
         
     phased_variants = pd.DataFrame(phased_variants, index=['patient_1', 'patient_2', 'mother_1', 'mother_2', 'father_1', 'father_2']).T 
@@ -492,9 +495,9 @@ def phase_duo_mode(patient_id, parent_id, genotypes_extracted, all_variants, del
     # phase variants with proxy    
     phased_variants = {}
     for variant_id in all_variants: 
-            phased_variants = phase_variant_duo(return_genotype(variant_id, genotypes_extracted[patient_id]['gt'],
+            phased_variants = phase_variant_duo(return_genotype(variant_id, genotypes_extracted[patient_id],
                                                                 is_patient_with_deletion=is_patient_with_deletion),
-                                                return_genotype(variant_id, genotypes_extracted[parent_id]['gt'],
+                                                return_genotype(variant_id, genotypes_extracted[parent_id],
                                                                 is_deletion_source=(parent_id == deletion_source)), phased_variants, variant_id)
 
     # multiallelic is not required since all get resolved with proxy and inheritance from patent rule 
@@ -534,7 +537,7 @@ if __name__ == '__main__':
     for s in samples: 
         vcfs[s] = read_vcf(os.path.join(args.input_folder, f'{s}{args.vcf_postfix}'))
         genotypes_extracted[s] = extract_genotypes(vcfs[s])
-        all_variants = all_variants + list(genotypes_extracted[s].index)
+        all_variants = all_variants + list(genotypes_extracted[s].keys())
         
     all_variants = list(set(all_variants))
 
@@ -579,15 +582,14 @@ if __name__ == '__main__':
             if pl_ch == 'match copy strategy':
                 # find deletion source and consider no variants inherited from deletion source 
                 # phase considering variants from deletion source 0/0 and variants from patient 0/1 
-                deletion_source = determine_deletions_source([patient_id, mother_id, father_id], genotypes_extracted, all_variants, ploidy)
-                is_patient_with_deletion = True
+                is_patient_with_deletion, deletion_source = determine_deletions_source([patient_id, mother_id, father_id], genotypes_extracted, all_variants, ploidy)
             else: 
                 deletion_source, is_patient_with_deletion = 'no', False
-            
+
             phased_variants = phase_trio_mode(patient_id, mother_id, father_id, genotypes_extracted, all_variants, deletion_source, is_patient_with_deletion)
-            
+         
             # if deletion was present, remove deletion source and patient from haplotypes since they are not phasable anyway  
-            if pl_ch == 'match copy strategy':
+            if is_patient_with_deletion:
                 drop_relative = 'mother' if deletion_source == mother_id else 'father'
                 phased_variants = phased_variants.drop(['patient_1', 'patient_2', f'{drop_relative}_1', f'{drop_relative}_2'], axis=1)
 
@@ -603,15 +605,14 @@ if __name__ == '__main__':
                 
             if pl_ch == 'match copy strategy':
                 # find deletion source and consider no variants inherited from deletion source 
-                deletion_source = determine_deletions_source([patient_id, parent_id], genotypes_extracted, all_variants, ploidy)
-                is_patient_with_deletion = True
+                is_patient_with_deletion, deletion_source = determine_deletions_source([patient_id, parent_id], genotypes_extracted, all_variants, ploidy)
             else: 
                 deletion_source, is_patient_with_deletion = 'no', False
 
             phased_variants = phase_duo_mode(patient_id, parent_id, genotypes_extracted, all_variants, deletion_source, is_patient_with_deletion)
             
             # if deletion was present, remove deletion source and patient from haplotypes since they are not phasable anyway  
-            if pl_ch == 'match copy strategy':
+            if is_patient_with_deletion:
                 drop_relative = 'parent' if deletion_source == parent_id else 'proxy'
                 phased_variants = phased_variants.drop(['patient_1', 'patient_2', f'{drop_relative}_1', f'{drop_relative}_2'], axis=1)
         
